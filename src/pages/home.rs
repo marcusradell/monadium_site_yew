18e032c8a0f657ec69ea4dd57cdc1985ca25e0ea
Marcus Rådell <marcus@radell.net>
2022-10-01 19:43:27 +0200

    add some info about monadium

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -4,7 +4,20 @@ pub fn home() -> Html {
     html! {
     <div>
         <p><em><strong>{"Helping hand for the junior developer."}</strong></em></p>
-        <h2>{"Join Us!"}</h2>
-        <p>{"Discord: "} <a href="https://discord.gg/59hgZycxYJ">{"https://discord.gg/59hgZycxYJ"}</a></p>
+
+        <section>
+            <h2>{"What's Monadium.org?"}</h2>
+            <p>{"It's a community where we try to make things a little better for developers and tech companies."}</p>
+
+            <h2>{"Who's running the show?"}</h2>
+            <p>{"My name is Marcus Rådell, and I'm a programmer who likes to help."}</p>
+            <p>{"You can find me as "}<strong>{"marcusradell"}</strong>{" on twitter, github, and linkedin. I'm "}<strong>{"ummonadi"}</strong>{" on reddit."}</p>
+
+            <h2>{"I want in!"}</h2>
+            <p>{"Here's the Discord chat server: "} <a href="https://discord.gg/59hgZycxYJ">{"https://discord.gg/59hgZycxYJ"}</a>{"."}</p>
+            <p>{"Throw us an introduction in the #introductions channel, or just talk in the #lounge."}</p>
+
+        </section>
+
     </div> }
 }
