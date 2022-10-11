a099fe422ec9707ed88afffc13303036d0695ae9
marcusradell <marcus@radell.net>
2022-10-11 11:53:58 +0000

    Deploying to gh-pages from @ marcusradell/monadium_site_yew@dcf9dca7bb5ba570c40e9816fd1079f2c0528a48 🚀

--- monadium_site_yew-49a474b3ff37ff52.js
+++ monadium_site_yew-49a474b3ff37ff52.js
@@ -799,7 +799,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('monadium_site_yew-d4a01a9ce8f7c5bf_bg.wasm', import.meta.url);
+        input = new URL('monadium_site_yew-49a474b3ff37ff52_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
